fe645f1bf82224efad52ca35aa7739ba972a8f95
Marto03 <marti775@abv.bg>
2023-10-15 19:26:24 +0300

    15.10.2023

---
+++ Uni/Lab/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String n = "w";
+        int n1 = 123;
+        n = Integer.toString(n1);
+        System.out.println(n); 
+        }
+}
